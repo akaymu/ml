047863277ad6f1b5b9fb1d7fe8918a0ca5981461
Muhlis Murat Akay <akaymu@gmail.com>
2021-12-30 15:45:37 +0300

    end of section 8

---
+++ regressions/index.js
@@ -0,0 +1,33 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
+
+let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower', 'weight', 'displacement'],
+  labelColumns: ['mpg'],
+});
+
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.1,
+  iterations: 100,
+});
+
+regression.train();
+const r2 = regression.test(testFeatures, testLabels);
+console.log(`R^2: ${r2}`);
+
+// plot({
+//   x: regression.bHistory,
+//   y: regression.mseHistory.reverse(),
+//   xLabel: 'Value of B',
+//   yLabel: 'Mean Squared Error',
+// });
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: 'Iteration #',
+  yLabel: 'Mean Squared Error',
+});
